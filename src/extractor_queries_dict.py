408f5d28be5ac7f3bc0f6dea96e38368ad22e5e3
Yaroslav Davydchuk <d.yarik@gmail.com>
2024-02-15 22:45:38 +0000

    finish all queries

--- src/extractor_queries_dict.py
+++ src/extractor_queries_dict.py
@@ -17,11 +17,11 @@ def get_query(table, last_successful_update_time):
             "design": f'''
             SELECT DISTINCT de.*
             FROM design de
-            INNER JOIN sales_order so
+            LEFT JOIN sales_order so
                 ON de.design_id = so.design_id
-            INNER JOIN transaction t
+            LEFT JOIN transaction t
                 ON so.sales_order_id = t.purchase_order_id
-            INNER JOIN payment pa
+            LEFT JOIN payment pa
                 ON t.transaction_id = pa.transaction_id
             WHERE de.last_updated >= '{last_successful_update_time}'
                 OR so.last_updated >= '{last_successful_update_time}'
@@ -29,103 +29,170 @@ def get_query(table, last_successful_update_time):
                 OR pa.last_updated >= '{last_successful_update_time}';''',
 
             "payment_type": f'''
-            SELECT * FROM payment_type
-            WHERE last_updated > '{last_successful_update_time}'
-            UNION
-            SELECT pt.* FROM payment_type pt
-            INNER JOIN payment p ON pt.payment_type_id = p.payment_type_id
-            WHERE p.last_updated > '{last_successful_update_time}';''',
+            SELECT DISTINCT pt.*
+            FROM payment_type pt
+            LEFT JOIN payment pa
+                ON pt.payment_type_id = pa.payment_type_id
+            WHERE pt.last_updated >= '{last_successful_update_time}'
+                OR pa.last_updated >= '{last_successful_update_time}';''',
 
             "payment": f'''
             SELECT * FROM payment
             WHERE last_updated > '{last_successful_update_time}';''',
 
             "currency": f'''
-            SELECT * FROM currency
-            WHERE last_updated >= '{last_successful_update_time}'
-            UNION
-            SELECT * FROM currency WHERE currency_id in
-            (SELECT currency_id
-            FROM payment
-            WHERE last_updated >= '{last_successful_update_time}')
-            UNION
-            (SELECT DISTINCT c.* FROM currency c
-            INNER JOIN purchase_order po ON c.currency_id = po.currency_id
-            INNER JOIN transaction t
-            ON po.purchase_order_id = t.purchase_order_id
-            WHERE T.created_at > '{last_successful_update_time}')
-            UNION
-            (SELECT DISTINCT c.* FROM currency c
-            INNER JOIN sales_order co ON c.currency_id = co.currency_id
-            INNER JOIN transaction t ON co.sales_order_id = t.sales_order_id
-            WHERE T.created_at > '{last_successful_update_time}');''',
+            SELECT DISTINCT cu.*
+            FROM currency cu
+            LEFT JOIN payment pa
+                ON cu.currency_id = pa.currency_id
+            WHERE cu.last_updated >= '{last_successful_update_time}'
+                OR pa.last_updated >= '{last_successful_update_time}'
 
-            # todo deps
+            UNION
+
+            SELECT DISTINCT cu.*
+            FROM currency cu
+            LEFT JOIN purchase_order po
+                ON cu.currency_id = po.currency_id
+            LEFT JOIN transaction t
+                ON po.purchase_order_id = t.purchase_order_id
+            LEFT JOIN payment pa
+                ON t.transaction_id = pa.transaction_id
+            WHERE cu.last_updated >= '{last_successful_update_time}'
+                OR po.last_updated >= '{last_successful_update_time}'
+                OR t.last_updated >= '{last_successful_update_time}'
+                OR pa.last_updated >= '{last_successful_update_time}'
+
+            UNION
+
+            SELECT DISTINCT cu.*
+            FROM currency cu
+            LEFT JOIN sales_order so
+                ON cu.currency_id = so.currency_id
+            LEFT JOIN transaction t
+                ON so.sales_order_id = t.purchase_order_id
+            LEFT JOIN payment pa
+                ON t.transaction_id = pa.transaction_id
+            WHERE cu.last_updated >= '{last_successful_update_time}'
+                OR so.last_updated >= '{last_successful_update_time}'
+                OR t.last_updated >= '{last_successful_update_time}'
+                OR pa.last_updated >= '{last_successful_update_time}';''',
+
             "department": f'''
-            SELECT * FROM department
-            WHERE last_updated >= '{last_successful_update_time}';''',
+            SELECT DISTINCT d.*
+            FROM department d
+            LEFT JOIN staff st
+                ON d.department_id = st.department_id
+            LEFT JOIN purchase_order po
+                ON st.staff_id = po.staff_id
+            LEFT JOIN transaction t
+                ON po.purchase_order_id = t.purchase_order_id
+            LEFT JOIN payment pa
+                ON t.transaction_id = pa.transaction_id
+            WHERE d.last_updated >= '{last_successful_update_time}'
+                OR st.last_updated >= '{last_successful_update_time}'
+                OR po.last_updated >= '{last_successful_update_time}'
+                OR t.last_updated >= '{last_successful_update_time}'
+                OR pa.last_updated >= '{last_successful_update_time}'
+
+            UNION
+
+            SELECT DISTINCT d.*
+            FROM department d
+            LEFT JOIN staff st
+                ON d.department_id = st.department_id
+            LEFT JOIN sales_order so
+                ON st.staff_id = so.staff_id
+            LEFT JOIN transaction t
+                ON so.sales_order_id = t.purchase_order_id
+            LEFT JOIN payment pa
+                ON t.transaction_id = pa.transaction_id
+            WHERE d.last_updated >= '{last_successful_update_time}'
+                OR st.last_updated >= '{last_successful_update_time}'
+                OR so.last_updated >= '{last_successful_update_time}'
+                OR t.last_updated >= '{last_successful_update_time}'
+                OR pa.last_updated >= '{last_successful_update_time}';''',
 
             "transaction": f'''
-            SELECT * FROM transaction
-            WHERE last_updated >= '{last_successful_update_time}';
-            UNION
-            SELECT t.* FROM transaction t
-            INNER JOIN payment p ON t.transaction_id = p.transaction_id
-            WHERE p.last_updated >= '{last_successful_update_time}';''',
+            SELECT DISTINCT tr.*
+            FROM transaction tr
+            LEFT JOIN payment pa
+                ON tr.transaction_id = pa.transaction_id
+            WHERE tr.last_updated >= '{last_successful_update_time}'
+                OR pa.last_updated >= '{last_successful_update_time}';''',
 
-            "address": f'''SELECT DISTINCT a.*
+            "address": f'''
+            SELECT DISTINCT a.*
             FROM address a
             LEFT JOIN counterparty c
-            ON a.address_id = c.legal_address_id
+               ON a.address_id = c.legal_address_id
             LEFT JOIN purchase_order po
-            ON c.counterparty_id = po.counterparty_id
+              ON c.counterparty_id = po.counterparty_id
             LEFT JOIN transaction t
-            ON po.purchase_order_id = t.purchase_order_id
-            LEFT JOIN payment pa ON t.transaction_id = pa.transaction_id
+                ON po.purchase_order_id = t.purchase_order_id
+            LEFT JOIN payment pa
+                ON t.transaction_id = pa.transaction_id
             WHERE a.last_updated >= '{last_successful_update_time}'
                 OR c.last_updated >= '{last_successful_update_time}'
                 OR po.last_updated >= '{last_successful_update_time}'
                 OR t.last_updated >= '{last_successful_update_time}'
                 OR pa.last_updated >= '{last_successful_update_time}'
+
             UNION
+
             SELECT DISTINCT a.*
             FROM address a
-            LEFT JOIN counterparty c ON a.address_id = c.legal_address_id
-            LEFT JOIN payment p ON c.counterparty_id = p.counterparty_id
+            LEFT JOIN counterparty c
+                ON a.address_id = c.legal_address_id
+            LEFT JOIN payment p
+                ON c.counterparty_id = p.counterparty_id
             WHERE a.last_updated >= '{last_successful_update_time}'
                 OR c.last_updated >= '{last_successful_update_time}'
                 OR p.last_updated >= '{last_successful_update_time}'
+
             UNION
+
             SELECT DISTINCT a.*
             FROM address a
-            LEFT JOIN counterparty c ON a.address_id = c.legal_address_id
-            LEFT JOIN sales_order so ON c.counterparty_id = so.counterparty_id
-            LEFT JOIN transaction t ON so.sales_order_id = t.sales_order_id
-            LEFT JOIN payment pa ON t.transaction_id = pa.transaction_id
+            LEFT JOIN counterparty c
+                ON a.address_id = c.legal_address_id
+            LEFT JOIN sales_order so
+                ON c.counterparty_id = so.counterparty_id
+            LEFT JOIN transaction t
+                ON so.sales_order_id = t.sales_order_id
+            LEFT JOIN payment pa
+                ON t.transaction_id = pa.transaction_id
             WHERE a.last_updated >= '{last_successful_update_time}'
                 OR c.last_updated >= '{last_successful_update_time}'
                 OR so.last_updated >= '{last_successful_update_time}'
                 OR t.last_updated >= '{last_successful_update_time}'
                 OR pa.last_updated >= '{last_successful_update_time}'
+
             UNION
+
             SELECT DISTINCT a.*
             FROM address a
             LEFT JOIN purchase_order po
-            ON a.address_id = po.agreed_delivery_location_id
+                  ON a.address_id = po.agreed_delivery_location_id
             LEFT JOIN transaction t
-            ON po.purchase_order_id = t.purchase_order_id
-            LEFT JOIN payment pa ON t.transaction_id = pa.transaction_id
+                ON po.purchase_order_id = t.purchase_order_id
+            LEFT JOIN payment pa
+                ON t.transaction_id = pa.transaction_id
             WHERE a.last_updated >= '{last_successful_update_time}'
                 OR po.last_updated >= '{last_successful_update_time}'
                 OR t.last_updated >= '{last_successful_update_time}'
                 OR pa.last_updated >= '{last_successful_update_time}'
+
             UNION
+
             SELECT DISTINCT a.*
             FROM address a
             LEFT JOIN sales_order so
                 ON a.address_id = so.agreed_delivery_location_id
-            LEFT JOIN transaction t ON so.sales_order_id = t.sales_order_id
-            LEFT JOIN payment pa ON t.transaction_id = pa.transaction_id
+            LEFT JOIN transaction t
+                ON so.sales_order_id = t.sales_order_id
+            LEFT JOIN payment pa
+                ON t.transaction_id = pa.transaction_id
             WHERE a.last_updated >= '{last_successful_update_time}'
                 OR so.last_updated >= '{last_successful_update_time}'
                 OR t.last_updated >= '{last_successful_update_time}'
@@ -134,24 +201,26 @@ def get_query(table, last_successful_update_time):
             "staff": f'''
             SELECT DISTINCT st.*
             FROM staff st
-            INNER JOIN purchase_order po
+            LEFT JOIN purchase_order po
                 ON st.staff_id = po.staff_id
-            INNER JOIN transaction t
+            LEFT JOIN transaction t
                 ON po.purchase_order_id = t.purchase_order_id
-            INNER JOIN payment pa
+            LEFT JOIN payment pa
                 ON t.transaction_id = pa.transaction_id
             WHERE st.last_updated >= '{last_successful_update_time}'
                 OR po.last_updated >= '{last_successful_update_time}'
                 OR t.last_updated >= '{last_successful_update_time}'
                 OR pa.last_updated >= '{last_successful_update_time}'
+
             UNION
+
             SELECT DISTINCT st.*
             FROM staff st
-            INNER JOIN sales_order so
+            LEFT JOIN sales_order so
                 ON st.staff_id = so.staff_id
-            INNER JOIN transaction t
+            LEFT JOIN transaction t
                 ON so.sales_order_id = t.purchase_order_id
-            INNER JOIN payment pa
+            LEFT JOIN payment pa
                 ON t.transaction_id = pa.transaction_id
             WHERE st.last_updated >= '{last_successful_update_time}'
                 OR so.last_updated >= '{last_successful_update_time}'
@@ -165,27 +234,31 @@ def get_query(table, last_successful_update_time):
                 ON co.counterparty_id = pa.counterparty_id
             WHERE co.last_updated >= '{last_successful_update_time}'
                 OR pa.last_updated >= '{last_successful_update_time}'
+
             UNION
+
             SELECT DISTINCT co.*
             FROM counterparty co
-            INNER JOIN purchase_order po
+            LEFT JOIN purchase_order po
                 ON co.counterparty_id = po.counterparty_id
-            INNER JOIN transaction t
+            LEFT JOIN transaction t
                 ON po.purchase_order_id = t.purchase_order_id
-            INNER JOIN payment pa
+            LEFT JOIN payment pa
                 ON t.transaction_id = pa.transaction_id
             WHERE co.last_updated >= '{last_successful_update_time}'
                 OR po.last_updated >= '{last_successful_update_time}'
                 OR t.last_updated >= '{last_successful_update_time}'
                 OR pa.last_updated >= '{last_successful_update_time}'
+
             UNION
+
             SELECT DISTINCT co.*
             FROM counterparty co
-            INNER JOIN sales_order so
+            LEFT JOIN sales_order so
                 ON co.counterparty_id = so.counterparty_id
-            INNER JOIN transaction t
+            LEFT JOIN transaction t
                 ON so.sales_order_id = t.purchase_order_id
-            INNER JOIN payment pa
+            LEFT JOIN payment pa
                 ON t.transaction_id = pa.transaction_id
             WHERE co.last_updated >= '{last_successful_update_time}'
                 OR so.last_updated >= '{last_successful_update_time}'
