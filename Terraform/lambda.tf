a019dc5f16794b42d2305c850f2693cc4dc70e25
Catamondium <30911915+Catamondium@users.noreply.github.com>
2024-02-20 13:00:39 +0000

    fix terraform ingestion
    ingestion wasn't recieving S3_CONTROL_BUCKET ENV var
    
    also collected utility bucket into a data source for tf attributes

--- Terraform/lambda.tf
+++ Terraform/lambda.tf
@@ -1,5 +1,5 @@
 resource "aws_lambda_function" "extraction_lambda" {
-    /*
+  /*
     Defines an AWS Lambda function for data extraction.
 
     Args:
@@ -21,25 +21,26 @@ resource "aws_lambda_function" "extraction_lambda" {
   role             = aws_iam_role.extraction_lambda_role.arn
   handler          = "extractor.lambda_handler"
   runtime          = "python3.11"
-  s3_bucket        = "rannoch-s3-utility-bucket"
+  s3_bucket        = data.aws_s3_bucket.utility_bucket.bucket
   s3_key           = "lambda-code/extraction_lambda.zip"
   layers           = ["arn:aws:lambda:eu-west-2:336392948345:layer:AWSSDKPandas-Python311:5"]
   source_code_hash = data.archive_file.lambda.output_base64sha256
-  memory_size = 256
-  timeout = 60
-  
+  memory_size      = 256
+  timeout          = 60
+
   lifecycle {
     replace_triggered_by = [null_resource.sauce]
   }
   environment {
     variables = {
       S3_EXTRACT_BUCKET = aws_s3_bucket.rannoch-s3-ingestion-bucket.bucket
-      PGUSER = "${var.username}"
-      PGPASSWORD = "${var.password}"
-      PGHOST = "${var.host}"
-      PGPORT = "${var.port}"
-      PGDATABASE = "${var.database}"
-      PG_LAST_UPDATED = "2000-01-01 00:00:00"
+      PGUSER            = "${var.username}"
+      PGPASSWORD        = "${var.password}"
+      PGHOST            = "${var.host}"
+      PGPORT            = "${var.port}"
+      PGDATABASE        = "${var.database}"
+      PG_LAST_UPDATED   = "2000-01-01 00:00:00"
+      S3_CONTROL_BUCKET = data.aws_s3_bucket.utility_bucket.bucket
     }
   }
 }
